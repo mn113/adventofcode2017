f772661465565885cbe4fe3bc3690e8766c69b65
Martin Nicholson <recyclebing+github@gmail.com>
2017-12-08 20:02:11 +0000

    Puzzle 08 Ruby

--- ruby/08.rb
+++ ruby/08.rb
@@ -1,25 +1,33 @@
 input = File.open("../inputs/08input.txt", "r")
 
 registers = Hash.new
+max = 0
 
 input.each_line do |line|
 	# Match the register names using regex:
 	/(?<key1>\w+) (inc|dec) \-?\d+ if (?<key2>\w+)/ =~  line.chomp
 
- 	next if !key1 || !key2
+  next if !key1 || !key2
 
 	# Initialise any new register keys:
 	registers[key1] = 0 if !registers.has_key?(key1)
 	registers[key2] = 0 if !registers.has_key?(key2)
 
-  	# Substitute += for inc, -= for dec, and wrap keys:
-  	eval line.chomp
-        	.gsub(/inc/, '+=')
-        	.gsub(/dec/, '-=')
-        	.gsub(/if/, '#')
-        	.gsub(/([a-z]+)/, 'registers[\'\1\']')
-        	.gsub(/#/, 'if')
+  # Substitute += for inc, -= for dec:
+  eval line.chomp
+          .gsub(/inc/, '+=')
+          .gsub(/dec/, '-=')
+          .gsub(/if/, '#')
+          .gsub(/([a-z]+)/, 'registers[\'\1\']')
+          .gsub(/#/, 'if')
+
+  high = registers.values.max
+  max = high if high > max
 end
 
-# Part 1: Find max
+p registers
+
+# Part 1: Find max at end
 p registers.values.max
+# Part 2: Find greatest val from start to end:
+p max
